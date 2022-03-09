c0c1a60c0517a43be49a1bb43d9f3abd7e68bb69
lin-zaw112 <minotosenpai@gmail.com>
2022-03-09 11:38:41 +0630

    theme color changer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,29 @@ const App = () => {
   const [theme, setTheme] = useState(true);
   const [Valid, setValid] = useState(true);
   const Strs = [...number, ...operator, theme ? Sun : Moon];
+  const r = document.querySelector(":root");
+  const nightThemeColor = new Map([
+    ["--BG", "#1d2d2e"],
+    ["--BGC", "#304649"],
+    ["--operator", "#4c6471"],
+    ["--BTN", "#e3fffa"],
+    ["--Text", "#000"],
+    ["--BGCl", "#334b4e"],
+    ["--Textfade", "#0000005f"],
+    ["--cont", "#d8cbcb"],
+    ["--equal", "#1c2826"],
+  ]);
+  const dayThemeColor = new Map([
+    ["--BG", "#D9F4ED"],
+    ["--BGC", "lightblue"],
+    ["--operator", "#96AFB8"],
+    ["--BTN", "#E5FDFF"],
+    ["--Text", "#063843"],
+    ["--BGCl", "#398F7E"],
+    ["--Textfade", "#ffffff90"],
+    ["--cont", "#000"],
+    ["--equal", "#ADD8E6"],
+  ]);
   const clickHandler = (event) => {
     event.preventDefault();
     const input = event.target.outerText;
@@ -28,23 +51,32 @@ const App = () => {
         }
         break;
       case "=":
-        {
-          if (isNaN(eval(sum.join("").replace(/x/g, "*")))) {
-            setValid(false);
-            return;
-          } else {
-            setsum(String(eval(sum.join("").replace(/x/g, "*"))));
-          }
+        if (isNaN(eval(sum.join("").replace(/x/g, "*")))) {
+          setValid(false);
+          return;
+        } else {
+          setsum(String(eval(sum.join("").replace(/x/g, "*"))));
         }
+
+        break;
+      case "":
+        break;
+      case undefined:
         break;
       default:
         setsum([...sum, input]);
         break;
     }
-    // const isValidHandler = (event) => {
-    //   setValid(event);
-    // };
   };
+  if (theme) {
+    dayThemeColor.forEach((value, key) => {
+      r.style.setProperty(key, value);
+    });
+  } else if (!theme) {
+    nightThemeColor.forEach((value, key) => {
+      r.style.setProperty(key, value);
+    });
+  }
   return (
     <React.Fragment>
       <div className={classes[`ripple-background`]}>
